208f00ac97eee195a973bd525925b84ab1afbadc
Ubuntu <ubuntu@ip-172-31-29-107.us-west-2.compute.internal>
2015-05-12 18:55:03 +0000

    Beeps working

--- peter3.js
+++ peter3.js
@@ -8,53 +8,60 @@ var ttsengine = "flite"; // TTS engine
 var ttsvoice = "slt"; // TTS voice                                                                                  
 var ttstimer = "soft"; // TTS timer type   
 
+var invocabot = false; // keeps track of whether last command was invocabot or not
+
 function ttsSpeak (session, phrase) {
-  session.speak(ttsengine, ttsvoice, phrase, ttstimer);
+	session.speak(ttsengine, ttsvoice, phrase, ttstimer);
 }
 
 
 function onInput(session, type, data, arg) {
 
-        var ttsengine = "flite"; // TTS engine                                                                      
-        var ttsvoice = "slt"; // TTS voice                                                                          
-        var ttstimer = "soft"; // TTS timer type                                                                   
+	var ttsengine = "flite"; // TTS engine                                                                      
+	var ttsvoice = "slt"; // TTS voice                                                                          
+	var ttstimer = "soft"; // TTS timer type                                                                   
 
-        var body = data.getBody();
-        body = body.replace(/<\?.*?\?>/g, '');
-        var xml = new XML("<xml>" + body + "</xml>");
-        var result = xml.getChild('result');
-        var command = "";
-        if (result)
-        {
-            command = result.getChild('interpretation').getChild('input').data;
-    	    if (command.indexOf('INVOCABOT') > -1) {
-    		  session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/siri_s.wav");
-    	    }
-        }
-        if (command != "") {
-        //session.execute("sleep", "2000");
-        // session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/siri_e.wav");
-        // var newCommand = "";
-        // newCommand = command.slice(10);
-        console_log("CONSOLE", "Command: " + command);
-        // if (command != "" )
-        // {
-        //     ttsSpeak(session, command);
-        // }
+	var body = data.getBody();
+	body = body.replace(/<\?.*?\?>/g, '');
+	var xml = new XML("<xml>" + body + "</xml>");
+	var result = xml.getChild('result');
+	var command = "";
 
-        e = new Event("custom", "message");
-        e.addBody(command);
-        e.fire();
+	if (result)
+	{
+		command = result.getChild('interpretation').getChild('input').data;
+		if (command.indexOf('INVOCABOT') > -1 && !invocabot) {
+			session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/siri_s.wav");
+			invocabot = true;
+		}
+		else if (command != "") {
+			//session.execute("sleep", "2000");
+			// newCommand = command.slice(10);
+			if (invocabot) {
+				session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/siri_e.wav");
+				invocabot = false;
+			}
+			console_log("CONSOLE", "Command: " + command);
+			// if (command != "" )
+			// {
+			//     ttsSpeak(session, command);
+			// }
 
-        session.execute("detect_speech", "resume");
-    }
-    return( false );
+			e = new Event("custom", "message");
+			e.addBody(command);
+			e.fire();
+
+		}
+
+		session.execute("detect_speech", "resume");
+	}
+	return( false );
 }
 
 function bridgeCallback ( session, type, dtmf, user_data) {
-        console_log("CONSOLE", "BEGINNING VOICE RECOGNITION");
-        //session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/silence.wav", onInput);
-        return false;
+	console_log("CONSOLE", "BEGINNING VOICE RECOGNITION");
+	//session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/silence.wav", onInput);
+	return false;
 }
 
 /***************** Begin Program *****************/
@@ -64,26 +71,26 @@ var originate_options = "ignore_early_media=true";
 
 session.execute("record_session", "/tmp/foo.wav")
 
-outSession = new Session("{"+originate_options+"}sofia/gateway/"+gateway+"/"+targetnumber);
+//outSession = new Session("{"+originate_options+"}sofia/gateway/"+gateway+"/"+targetnumber);
 
 
 
 if (session.ready()){
-        session.answer();
-        session.execute("sleep", "5000");
-        session.execute("detect_speech", "pocketsphinx invocabot invocabot");
-        session.execute("divert_events", "on");
-        
-        if (outSession.ready()) {
-        //     outSession.answer();
-        //bridge(session, outSession, bridgeCallback);
-	    // ttsSpeak(outSession, "The call is currently being recorded");
-     //    console_log("CONSOLE", "The call is currently being recorded");
+	session.answer();
+	session.execute("sleep", "5000");
+	session.execute("detect_speech", "pocketsphinx invocabot invocabot");
+	session.execute("divert_events", "on");
 
-        while (session.ready()) {
-          session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/silence.wav", onInput); 
-        }
-    }
-    
+	//	if (outSession.ready()) {
+	//     outSession.answer();
+	//bridge(session, outSession, bridgeCallback);
+	// ttsSpeak(outSession, "The call is currently being recorded");
+	//    console_log("CONSOLE", "The call is currently being recorded");
+
+	while (session.ready()) {
+		session.streamFile("/usr/local/freeswitch/sounds/en/us/invocabot/silence.wav", onInput); 
+	}
+	//	}
+
 
 }
